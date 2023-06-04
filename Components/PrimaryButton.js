9d14626c8b44834649cfbbfd52a1c4eca82991f2
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-06-04 15:25:53 +0100

    styling the button

--- Components/PrimaryButton.js
+++ Components/PrimaryButton.js
@@ -5,8 +5,16 @@ function PrimaryButton({ children }) {
     console.log("pressed ");
   }
   return (
-    <View style={styles.buttonInnerContainer}>
-      <Pressable onPress={pressHandler} android_ripple={{ color: "#640233" }}>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
     </View>
@@ -23,14 +31,15 @@ const styles = StyleSheet.create({
   },
   buttonInnerContainer: {
     backgroundColor: "#72063c",
-
     paddingVertical: 8,
     paddingHorizontal: 16,
-
     elevation: 2,
   },
   buttonText: {
     color: "white",
     textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
