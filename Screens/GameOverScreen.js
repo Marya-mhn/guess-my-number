01febd9635abcf1c497f9ac40ddf2cdae3119e56
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-06-07 15:26:07 +0100

    styling

--- Screens/GameOverScreen.js
+++ Screens/GameOverScreen.js
@@ -1,13 +1,26 @@
 import { Text, View, Image, StyleSheet } from "react-native";
 import Title from "../Components/ui/Title";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
   return (
     <View>
       <Title>GAME OVER</Title>
-      <Image source={require("../assets/success.png")} />
+      <View>
+        <Image source={require("../assets/success.png")} />
+      </View>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    width: 400,
+    height: 400,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+  },
+});
